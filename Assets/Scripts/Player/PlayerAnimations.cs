87237aacf837f42ea95f72c9659d953b166856a0
KR3MIT <jason.cm.hassan@gmail.com>
2026-02-06 15:47:09 +0100

    qwedaerfgsrdtyfhmgj,k,

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,66 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    public float animSmoothSpeed = 10f;
+
+    private Animator animator;
+    private PlayerInput input;
+    private CharacterController controller;
+    private PlayerMovement playerMovement;
+
+    private float smoothMoveX;
+    private float smoothMoveY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Initialize();
+        Inputs();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementAnim();
+    }
+
+    private void Initialize()
+    {
+        animator = GetComponentInChildren<Animator>();
+        input = GetComponent<PlayerInput>();
+        controller = GetComponent<CharacterController>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void Inputs()
+    {
+        input.actions["Jump/Climb"].performed += ctx => Jump();
+    }
+
+    private void Jump()
+    {
+        if(controller.isGrounded)
+            animator.SetTrigger("Jump");
+    }
+
+    private void MovementAnim()
+    {
+        animator.SetBool("IsGrounded", controller.isGrounded);
+
+        Vector2 inputVector = input.actions["Movement"].ReadValue<Vector2>();
+
+        if(inputVector.magnitude > 0.1f)
+            animator.SetBool("IsMoving", true);
+        else
+            animator.SetBool("IsMoving", false);
+
+        smoothMoveX = Mathf.Lerp(smoothMoveX, inputVector.x, animSmoothSpeed * Time.deltaTime);
+        smoothMoveY = Mathf.Lerp(smoothMoveY, inputVector.y, animSmoothSpeed * Time.deltaTime);
+
+        animator.SetFloat("MoveX", smoothMoveX);
+        animator.SetFloat("MoveY", smoothMoveY);
+    }
+}
